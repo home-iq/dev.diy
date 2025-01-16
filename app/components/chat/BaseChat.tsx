ddd9248d9732c35e9a969c9e2655145c5d409b0b
jonthewayne <jonthewayne@gmail.com>
2025-01-15 19:14:02 -0800

    more ui

--- app/components/chat/BaseChat.tsx
+++ app/components/chat/BaseChat.tsx
@@ -511,7 +511,7 @@ export const BaseChat = React.forwardRef<HTMLDivElement, BaseChatProps>(
                         minHeight: TEXTAREA_MIN_HEIGHT,
                         maxHeight: TEXTAREA_MAX_HEIGHT,
                       }}
-                      placeholder="How can Bolt help you today?"
+                      placeholder="How can myhomeIQ bolt.diy help you today?"
                       translate="no"
                     />
                     <ClientOnly>
